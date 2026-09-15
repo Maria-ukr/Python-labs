87976735766d514d76bc4a0e7f99b665d3c88342
Maria <mari.mikhaleva.ukr@gmail.com>
2026-09-15 17:33:53 +0300

    second laba

--- main.py
+++ main.py
@@ -1,73 +1,47 @@
-# First program
-a_input = input("Enter first number: ")
+# First task Скласти програму, яка визначає і виводить на екран кількість слів у реченні, які закінчуються на літеру «р».
 
-while not (a_input.isdigit() and int(a_input) > 0):
-  a_input = input("Please enter a non-negative first number: ")
-a = int(a_input)
+# Jump up and stop at top level, Philip.
 
-b_input = input("Enter second number: ")
+found_letter = "p"
+str_with_found_letter = ""
+str = ""
 
-while not (b_input.isdigit() and int(b_input) > 0):
-  b_input = input("Please enter a non-negatlive second number: ")
-b = int(b_input)
+while (len(str.split(' ')) < 7):
+  str = input("Enter a string with at least 7 characters: ")
 
-if a == b:
-  result = -1
+split_str = str.split(' ')
+for word in split_str:
+  if word.endswith(found_letter):
+    str_with_found_letter += word + ' '
 
-elif a < b:
-  result = a/b + 1
+print(str_with_found_letter)    
+print(len(str_with_found_letter.split(' ')))
 
+
+
+# Second task Видалення зі списку повторень.
+
+import re
+
+inputtingSchedule = input("Enter a schedule: ")
+if len(inputtingSchedule) <= 0:
+  inputtingSchedule = "Ukraine, Poland, Germany, France, Italy, Spain, Ukraine, Poland"
+
+list = re.findall("\w+", inputtingSchedule)
+
+print(inputtingSchedule)
+print(set(list))
+
+
+# Third task Задано множини символiв А i символ «х». Скласти програму, яка формує множину В з множини А за таким правилом: а) додавання елемента х, якщо він відсутній в А; б) видалення елемента х, якщо він наявний в А.
+
+set_a = {'a', 'b', 'c', 'd', 'e'}
+set_b = set(set_a)
+symbol_x = 'x'
+
+if symbol_x not in set_a:
+  set_b.add(symbol_x)
 else:
-  result = (a * b - 5) / a
+  set_b.discard(symbol_x)
 
-print("The result is:", result)
-
-
-
-
-
-
-# Second program
-n_input = input("Enter a number from 1 to 10: ")
-
-while not (n_input.isdigit() and int(n_input) > 0 and int(n_input) < 10):
-  n_input = input("Please enter a number from 1 to 10: ")
-
-N = int(n_input)
-
-for i in range(N, 0, -1):
-  for j in range(0, N, 1):
-    if j > i - 1:
-      print(" ", end=" ")
-    else:
-      print(N, end = " ")
-  print("")
-
-for i in range(1, N + 1, 1):
-  for j in range(0, N, 1):
-    if j > i - 1:
-      print(" ", end=" ")
-    else:
-      print(N, end = " ")
-  print("")
-
-
-
-
-
-
-# Third program
-from mod import countingNumbers
-
-x_input = input("Enter a positive number: ")
-
-while not (x_input.isdigit() and int(x_input) > 0):
-  x_input = input("Please enter a positive number: ")
-
-countingNumbers(int(x_input))
-
-n = 0
-while n <= 20:
-  if n % 2 == 0 and not n == 0:
-    print(n)
-  n += 1
+print(set_b)
--- mod.py
+++
@@ -1,5 +0,0 @@
-import math
-
-def countingNumbers(n):
-  result = math.log(n + 1) + math.sqrt(n)
-  print("The result is:", result)
